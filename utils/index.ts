2dc936bf9e68c798694b70bfb40a49c96768eced
George <alximik15@gmail.com>
2023-09-01 11:32:06 +0400

    Created a fetchCars request

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+
+
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "425e820309msh4df3295b51039f8p127a40jsn724185e40791",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+    {
+      headers: headers,
+    });
+	const result = await response.json();
+
+	return result
+}
