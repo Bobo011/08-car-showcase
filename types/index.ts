446893b190ed57f7b83a16310a5ad4658b3ec3c0
George <alximik15@gmail.com>
2023-09-01 19:28:28 +0400

    Fetching Cars Updated

--- types/index.ts
+++ types/index.ts
@@ -31,3 +31,11 @@ export interface CarProps {
     year: number;
  
 }
+
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,19 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
 
-export async function fetchCars() {
+export async function fetchCars(filters:FilterProps) {
+ const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "X-RapidAPI-Key": "425e820309msh4df3295b51039f8p127a40jsn724185e40791",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
-    });
+    }
+  );
 	const result = await response.json();
 
 	return result
