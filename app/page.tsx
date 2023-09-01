b313fcc407d1c810012e983c3917a3f5891444c5
George <alximik15@gmail.com>
2023-09-01 20:12:23 +0400

    Custom Filters are Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { CustomFilter, CarCard, Hero, SearchBar } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
@@ -18,42 +19,39 @@ export default async function Home({searchParams}) {
 
 
   return (
-    <main className='overflow-hidden'>
-
+    <main className="overflow-hidden">
       <Hero />
 
-      <div className='mt-12 padding-x padding-y max-width' id='discover'>
-        <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'> Car Catalogue</h1>
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold"> Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
 
-        <div className='home__filters'>
+        <div className="home__filters">
           <SearchBar />
 
-          <div className='home__filer-container'>
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+          <div className="home__filer-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
         {!isDataEmpty ? (
           <section>
-            <div className='home__cars-wrapper'>
+            <div className="home__cars-wrapper">
               {allCars?.map((car) => (
                 <CarCard car={car} />
               ))}
             </div>
           </section>
-
         ) : (
-          <div className='home__error-container'>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
       </div>
     </main>
-
-  )
+  );
 }
