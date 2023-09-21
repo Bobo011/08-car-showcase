f8836de032de9f36f08c80f302488e7586bf5118
George <alximik15@gmail.com>
2023-09-21 13:27:02 +0400

    Finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-// Import necessary components and data from external files/modules.
-import { CustomFilter, CarCard, Hero, SearchBar, ShowMore } from "@/components"; // Import custom components from '@/components' directory.
-import { fuels, yearsOfProduction } from "@/constants"; // Import fuel and yearsOfProduction data from '@/constants'.
-import { HomeProps } from "@/types"; // Import HomeProps type from '@/types'.
-import { fetchCars } from "@/utils"; // Import fetchCars function from '@/utils'.
 
-// Define the Home function component that receives searchParams as a prop.
+import { CustomFilter, CarCard, Hero, SearchBar, ShowMore } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
+import { HomeProps } from '@/types';
+import { fetchCars } from '@/utils'
+
+
+
+
 export default async function Home({ searchParams }: HomeProps) {
-  // Fetch cars based on search parameters using the fetchCars function.
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
@@ -15,40 +16,35 @@ export default async function Home({ searchParams }: HomeProps) {
     model: searchParams.model || "",
   });
 
-  // Check if the fetched data is empty.
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
-  // Render the main content of the home page.
   return (
     <main className="overflow-hidden">
-      <Hero /> {/* Render the Hero component at the top of the page. */}
+      <Hero />
+
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
-          <p>Explore our cars you might like</p>
+          <p>Explore out cars you might like</p>
         </div>
 
         <div className="home__filters">
-          <SearchBar /> {/* Render the SearchBar component. */}
+          <SearchBar />
+
           <div className="home__filter-container">
-            <CustomFilter title="fuel" options={fuels} />{" "}
-            {/* Render CustomFilter for fuel options. */}
-            <CustomFilter title="year" options={yearsOfProduction} />{" "}
-            {/* Render CustomFilter for year options. */}
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
-        {/* Check if there are cars to display. */}
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
-              {/* Map through allCars and render CarCard components for each car. */}
               {allCars?.map((car) => (
-                <CarCard car={car} key={car.id} />
+                <CarCard car={car} />
               ))}
             </div>
 
-            {/* Render ShowMore component for pagination. */}
             <ShowMore
               pageNumber={(searchParams.limit || 10) / 10}
               isNext={(searchParams.limit || 10) > allCars.length}
